0ad8862dc4904fa80c77aedf0d08db1a7f76b8de
OlsenFischer <151182208+OlsenFischer@users.noreply.github.com>
2023-11-28 16:43:03 +0300

    Loropio App

--- LoropioFitnessApp.console/Program.cs
+++ LoropioFitnessApp.console/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Chapter 2!");
+
+Console.WriteLine("What is your name? ");
+string name = Console.ReadLine();
+
+Console.WriteLine("Hello " + name + ", Welcome to Loropio Fitness App");
